3f4d9a8ae605f71ab57461b6e5b754470d70c1cb
Mikkel Amskov <mikkel@amskov.dk>
2023-10-23 10:10:10 +0200

    Her

--- app/src/main/java/com/example/mediaappniklas2/MainActivity.kt
+++ app/src/main/java/com/example/mediaappniklas2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mediaappniklas2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,11 +11,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -64,37 +70,48 @@ fun Medieknapper(){
     MediaAppNiklas2Theme {
         Column {
             Row {
-                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
-                    Text(text = "Netflix"
-                    , fontSize = 10.sp)
+                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)) {
+                    Text(text = "Netflix", fontSize = 10.sp, textAlign = TextAlign.Center)
                 }
-                Button(onClick = { /*TODO*/ }, Modifier.size(85.dp,40.dp)) {
+                Button(onClick = { /*TODO*/ }, Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Magenta)) {
                     Text(text = "Viaplay"
-                        , fontSize = 10.sp)
+                        , fontSize = 10.sp
+                    ,textAlign = TextAlign.Center)
                 }
-                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
+                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)) {
                     Text(text = "HBO"
-                        , fontSize = 10.sp)
+                        , fontSize = 10.sp
+                    ,textAlign = TextAlign.Center)
                 }
+
             }
             Spacer(modifier = Modifier.height(5.dp))
             Row {
-                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
-                    Text(text = "Disney +"
-                        , fontSize = 10.sp)
+                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)) {
+                    Text(text = "Disney+"
+                        , fontSize = 10.sp
+                    ,textAlign = TextAlign.Center)
                 }
-                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
+                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Black)) {
                     Text(text = "Apple tv"
-                        , fontSize = 10.sp)
+                        , fontSize = 10.sp
+                    ,textAlign = TextAlign.Center)
                 }
                 Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
-                    Text(text = "Prime video"
-                        , fontSize = 10.sp)
+                    Text(text = "Prime"
+                        , fontSize = 10.sp
+                    ,textAlign = TextAlign.Center)
                 }
 
             }
 
-        }
+
 
     }
+}
 }
