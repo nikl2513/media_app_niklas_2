b71eb50e2c0f5233d04eedafd56f022a6102c3bf
nikl2513 <nikl2513@outlook.dk>
2023-10-23 10:07:03 +0200

    baggrund 3

--- app/src/main/java/com/example/mediaappniklas2/Opstart.kt
+++ app/src/main/java/com/example/mediaappniklas2/Opstart.kt
@@ -2,9 +2,12 @@ package com.example.mediaappniklas2
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
@@ -16,6 +19,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,6 +41,7 @@ fun OpstartMedButtonOgBaggrund(
             Alignment.BottomCenter
         )
 ) {
+    Image(painter = painterResource(id = R.drawable.background1), contentDescription = "", modifier.fillMaxSize(), contentScale = ContentScale.Crop)
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Button(
             onClick = { /*TODO*/ },
