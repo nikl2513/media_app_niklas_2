82503d9cd97d765cd98dee9d4145b43064a3e1c2
Nikolai Travis <s224567@student.dtu.dk>
2023-10-23 10:32:42 +0200

    Overflytning

--- app/src/main/java/com/example/mediaappniklas2/Startskærm.kt
+++ app/src/main/java/com/example/mediaappniklas2/Startskærm.kt
@@ -1,11 +1,68 @@
 package com.example.mediaappniklas2
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mediaappniklas2.ui.theme.MediaAppNiklas2Theme
 
-class Startskærm {
+@Preview
+@Composable
+fun medieKnapper(){
+    MediaAppNiklas2Theme {
+        Column {
+            Row {
+                Button(onClick = { /*TODO*/ }, Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)) {
+                    Text(text = "Netflix", fontSize = 10.sp, textAlign = TextAlign.Center)
+                }
+                Button(onClick = { /*TODO*/ }, Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Magenta)) {
+                    Text(text = "Viaplay"
+                        , fontSize = 10.sp
+                        ,textAlign = TextAlign.Center)
+                }
+                Button(onClick = { /*TODO*/ }, Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)) {
+                    Text(text = "HBO"
+                        , fontSize = 10.sp
+                        ,textAlign = TextAlign.Center)
+                }
 
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+            Row {
+                Button(onClick = { /*TODO*/ }, Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)) {
+                    Text(text = "Disney+"
+                        , fontSize = 10.sp
+                        ,textAlign = TextAlign.Center)
+                }
+                Button(onClick = { /*TODO*/ }, Modifier.size(85.dp,40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Black)) {
+                    Text(text = "Apple tv"
+                        , fontSize = 10.sp
+                        ,textAlign = TextAlign.Center)
+                }
+                Button(onClick = { /*TODO*/ }, Modifier.size(85.dp,40.dp)) {
+                    Text(text = "Prime"
+                        , fontSize = 10.sp
+                        ,textAlign = TextAlign.Center)
+                }
 
+            }
 
+        }
+    }
 }
