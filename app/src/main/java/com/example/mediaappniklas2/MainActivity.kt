31ec8d6ba289990d91e83c6b5066051c38d3cacf
Mikkel Amskov <mikkel@amskov.dk>
2023-10-23 09:30:23 +0200

    Her

--- app/src/main/java/com/example/mediaappniklas2/MainActivity.kt
+++ app/src/main/java/com/example/mediaappniklas2/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.mediaappniklas2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mediaappniklas2.ui.theme.MediaAppNiklas2Theme
 
 class MainActivity : ComponentActivity() {
@@ -37,10 +45,57 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Movieknapper() {
+    MediaAppNiklas2Theme{
+        listOf(
+            Button(onClick = { /*TODO*/ }) {
+            }
+        )
+    }
+}
+
+
+
+@Preview
+@Composable
+fun Medieknapper(){
     MediaAppNiklas2Theme {
-        Greeting("Android")
+        Column {
+            Row {
+                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
+                    Text(text = "Netflix"
+                    , fontSize = 10.sp)
+                }
+                Button(onClick = { /*TODO*/ }, Modifier.size(85.dp,40.dp)) {
+                    Text(text = "Viaplay"
+                        , fontSize = 10.sp)
+                }
+                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
+                    Text(text = "HBO"
+                        , fontSize = 10.sp)
+                }
+            }
+            Spacer(modifier = Modifier.height(5.dp))
+            Row {
+                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
+                    Text(text = "Disney +"
+                        , fontSize = 10.sp)
+                }
+                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
+                    Text(text = "Appel tv"
+                        , fontSize = 10.sp)
+                }
+                Button(onClick = { /*TODO*/ },Modifier.size(85.dp,40.dp)) {
+                    Text(text = "Prime video"
+                        , fontSize = 10.sp)
+                }
+
+            }
+
+
+        }
+
     }
 }
