b94229f87ea14a53e80cf7b9d64044d65480abbd
Jason <Jasonyuksel@hotmail.com>
2023-10-23 09:18:38 +0200

    Loginside lavet

---
+++ app/src/main/java/com/example/mediaappniklas2/ui/theme/Loginactivity.kt
@@ -0,0 +1,12 @@
+package com.example.mediaappniklas2.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mediaappniklas2.R
+
+class Loginactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginactivity)
+    }
+}
