c4ab42322efc38c5dbd67f80e9134e8c6388eb87
Nikolai Travis <s224567@student.dtu.dk>
2023-10-23 10:55:29 +0200

    fun OpstartStartskærm() V2

--- app/src/main/java/com/example/mediaappniklas2/Startskærm.kt
+++ app/src/main/java/com/example/mediaappniklas2/Startskærm.kt
@@ -3,12 +3,15 @@ package com.example.mediaappniklas2
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -16,13 +19,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mediaappniklas2.ui.theme.MediaAppNiklas2Theme
+@Preview
 @Composable
 fun OpstartStartskærm() {
     MedieKnapper()
 }
-@Preview
 @Composable
-fun MedieKnapper(){
+fun MedieKnapper(modifier: Modifier = Modifier .fillMaxSize() .wrapContentSize(Alignment.Center)){
     MediaAppNiklas2Theme {
         Column {
             Row {
