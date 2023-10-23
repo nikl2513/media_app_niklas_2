8222c2dab13aabbae3da91a1997c7fc69fde85a9
nikl2513 <nikl2513@outlook.dk>
2023-10-23 10:57:30 +0200

    baggrund 4

--- app/src/main/java/com/example/mediaappniklas2/Opstart.kt
+++ app/src/main/java/com/example/mediaappniklas2/Opstart.kt
@@ -3,13 +3,17 @@ package com.example.mediaappniklas2
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -18,12 +22,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mediaappniklas2.Greeting
 import com.example.mediaappniklas2.R
 
@@ -43,19 +51,29 @@ fun OpstartMedButtonOgBaggrund(
 ) {
     Image(painter = painterResource(id = R.drawable.background1), contentDescription = "", modifier.fillMaxSize(), contentScale = ContentScale.Crop)
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(id = R.drawable.logo), contentDescription = "")
+        Image(painter = painterResource(id = R.drawable.logotext), contentDescription = "", modifier = Modifier .size(300.dp))
         Button(
             onClick = { /*TODO*/ },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+            shape = RoundedCornerShape(5.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+            modifier = Modifier
+                .height(50.dp)
+                .width(300.dp)
         ) {
-            Text(stringResource(id = R.string.tilmeld))
+            Text(stringResource(id = R.string.tilmeld), fontSize = 20.sp)
         }
-        Spacer(modifier = Modifier.height(25.dp))
+        Spacer(modifier = Modifier.height(30.dp))
         Button(onClick = { /*TODO*/ },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue))
+            shape = RoundedCornerShape(5.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+            modifier = Modifier
+                .height(50.dp)
+                .width(300.dp))
             {
-            Text(stringResource(id = R.string.logind))
+            Text(stringResource(id = R.string.logind), fontSize = 20.sp)
         }
-        Spacer(modifier = Modifier.height(150.dp))
+        Spacer(modifier = Modifier.height(100.dp))
     }
 
 }
