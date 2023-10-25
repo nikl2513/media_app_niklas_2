5dcdf017aa664fafed154d11ab279bd514d1bf17
Jason <Jasonyuksel@hotmail.com>
2023-10-25 22:26:26 +0200

    Login side næsten færdig, mangler kun baggrundsændrelse

--- app/src/main/java/com/example/mediaappniklas2/interfaces/LoginForm.kt
+++ app/src/main/java/com/example/mediaappniklas2/interfaces/LoginForm.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -39,8 +40,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -48,6 +51,7 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mediaappniklas2.MainActivity
+import com.example.mediaappniklas2.R
 import com.example.mediaappniklas2.ui.theme.MediaAppNiklas2Theme
 
 @Composable
