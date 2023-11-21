857c15bd485588d649f3614367c62fad48b90875
nikl2513 <nikl2513@outlook.dk>
2023-11-21 15:27:31 +0100

    ændring i movietitle

--- app/src/main/java/com/example/mediaappniklas2/GradientButton.kt
+++ app/src/main/java/com/example/mediaappniklas2/GradientButton.kt
@@ -1,6 +1,7 @@
 package com.example.mediaappniklas2
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,17 +17,25 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mediaappniklas2.ui.theme.MediaAppNiklas2Theme
 
 @Composable
-fun GradientButton(){
+fun GradientButton(modifier: Modifier = Modifier){
 
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ){
+        Image(
+            painter = painterResource(id = R.drawable.background1),
+            contentDescription = "",
+            modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
 
     Column(
         verticalArrangement = Arrangement.Center,
