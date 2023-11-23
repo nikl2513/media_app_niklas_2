685ec002572da299f5296f056a9f4ceca87c81e5
MathiasLinde <113913029+MathiasLinde@users.noreply.github.com>
2023-11-23 13:57:12 +0100

    Merged DataHandling into Dev branch

--- app/src/main/java/com/example/mediaappniklas2/presentation/mediapage/MediaPage.kt
+++ app/src/main/java/com/example/mediaappniklas2/presentation/mediapage/MediaPage.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
@@ -69,6 +68,7 @@ fun MediaPage(modifier: Modifier = Modifier)
         )
         Text(movieid.Title)
         Text(movieid.releasedate)
+
     }
 
 }
