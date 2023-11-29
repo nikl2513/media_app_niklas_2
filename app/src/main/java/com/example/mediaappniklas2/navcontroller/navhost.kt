5cb8887309e6bc62483feec6fe5cf48fe60dc617
Mikkel Amskov <mikkel@amskov.dk>
2023-11-29 19:23:52 +0100

    nav til Search

--- app/src/main/java/com/example/mediaappniklas2/navcontroller/navhost.kt
+++ app/src/main/java/com/example/mediaappniklas2/navcontroller/navhost.kt
@@ -11,6 +11,7 @@ import com.example.mediaappniklas2.GradientButton
 import com.example.mediaappniklas2.domainLayer.MovieUtils
 import com.example.mediaappniklas2.presentation.Login.LoginForm
 import com.example.mediaappniklas2.presentation.Opstart.OpstartMedButtonOgBaggrund
+import com.example.mediaappniklas2.presentation.Search.SearchBar
 import com.example.mediaappniklas2.presentation.mediapage.MediaPageAPP
 import com.example.mediaappniklas2.presentation.mediapage.MediaPageViewModel
 import com.example.mediaappniklas2.presentation.startskærm.HomePageViewModel
@@ -45,6 +46,9 @@ fun NavHost() {
         }
         composable(route = Screen.GradientButton.route){
             GradientButton(navController = navController)
+        }
+        composable(route = Screen.Search.route){
+            SearchBar(navController = navController)
         }
     }
 }
