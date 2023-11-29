f2b1cb09c59eec6d44f2e6566805eb2457f0b984
Mikkel Amskov <mikkel@amskov.dk>
2023-11-29 17:52:56 +0100

    Knap

--- app/src/main/java/com/example/mediaappniklas2/presentation/Search/Search.kt
+++ app/src/main/java/com/example/mediaappniklas2/presentation/Search/Search.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -22,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -31,17 +33,28 @@ import com.example.mediaappniklas2.navcontroller.Screen
 
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun SearchBar(navController: NavController) {
-
+fun SearchBar() {
     var text by remember { mutableStateOf("") }
-    TextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("Search") },
-        leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
-        modifier = Modifier.fillMaxWidth()
-    )
+    Column {
+
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Search") },
+            leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
+            modifier = Modifier.fillMaxWidth()
+
+        )
+        Button(onClick = { SearchPageViewModel.searchMovieInAPI(text) }) {
+            Text(text = "Search")
+            
+        }
+
+
+
+    }
 
 
 
--- app/src/main/java/com/example/mediaappniklas2/presentation/Search/SearchPageViewModel.kt
+++ app/src/main/java/com/example/mediaappniklas2/presentation/Search/SearchPageViewModel.kt
@@ -17,31 +17,35 @@ import kotlinx.coroutines.flow.stateIn
 
 class SearchPageViewModel: ViewModel() {
 
-    suspend fun searchMovieInAPI(searchword : String ) : List<MovieData>{
-        val movieApiResponse: MovieApiResponse = RetrofitClient.movieApiService.searchmovies(searchword)
+        suspend fun searchMovieInAPI(searchword: String): List<MovieData> {
+            val movieApiResponse: MovieApiResponse =
+                RetrofitClient.movieApiService.searchmovies(searchword)
 
-        // Process the response as before
-        val resultsList: List<MovieDTO> = movieApiResponse.results
-
-
-        val movieList  = resultsList.map { convertToMovieData(it) }
+            // Process the response as before
+            val resultsList: List<MovieDTO> = movieApiResponse.results
 
 
-    suspend fun searchMovieInAPI(searchword : String ) : List<MovieData>{
-        val movieApiResponse: MovieApiResponse = RetrofitClient.movieApiService.searchmovies(searchword)
-
-        // Process the response as before
-        val resultsList: List<MovieDTO> = movieApiResponse.results
+            val movieList = resultsList.map { convertToMovieData(it) }
 
 
-        val movieList  = resultsList.map { convertToMovieData(it) }
+            return movieList
+        }
+
+    companion object {
+        suspend fun searchMovieInAPI(searchword: String): List<MovieData> {
+            val movieApiResponse: MovieApiResponse =
+                RetrofitClient.movieApiService.searchmovies(searchword)
+
+            // Process the response as before
+            val resultsList: List<MovieDTO> = movieApiResponse.results
 
 
+            val movieList = resultsList.map { convertToMovieData(it) }
 
-        return movieList
+
+            return movieList
+        }
     }
-
-
 
 
 }
