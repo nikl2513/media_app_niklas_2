2d804fb108cf066e387ded8c920006e552b6b5e2
nikl2513 <nikl2513@outlook.dk>
2023-11-29 20:56:49 +0100

    changes in mediapage

--- app/src/main/java/com/example/mediaappniklas2/presentation/mediapage/MediaPage.kt
+++ app/src/main/java/com/example/mediaappniklas2/presentation/mediapage/MediaPage.kt
@@ -1,19 +1,30 @@
 package com.example.mediaappniklas2.presentation.mediapage
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
@@ -61,15 +72,28 @@ fun MediaPage(modifier: Modifier = Modifier, movieViewModel: MediaPageViewModel)
         modifier = modifier
             .fillMaxSize()
             .wrapContentSize(Alignment.Center),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.Center,
         verticalArrangement = Arrangement.Center
     ) {
         currentMovie?.let { movie ->
-            Image(
-                painter = rememberAsyncImagePainter(movie.imageRef),
-                contentDescription = "A movie poster",
-                modifier = Modifier.size(500.dp)
-            )
+            /*Box(
+                modifier
+                    .size(300.dp, 380.dp)
+                    .background(Color.DarkGray)
+                    .clip(shape = RoundedCornerShape(10.dp))
+            ) {*/
+                val imageModifier = Modifier
+                    .width(350.dp)
+                    .height(400.dp)
+                    .border(BorderStroke(1.dp, Color.Black))
+                    .background(Color.Yellow)
+                Image(
+                    painter = rememberAsyncImagePainter(movie.imageRef),
+                    contentDescription = "A movie poster",
+                    contentScale = ContentScale.Crop,
+                    modifier = imageModifier
+                )
+            //}
             Text(movie.title)
             Text(movie.releasedate)
         }
