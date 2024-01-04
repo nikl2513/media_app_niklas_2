c66e9c9bad6b3d8d08d7c890b16302c0a4925fbf
Mikkel Amskov <mikkel@amskov.dk>
2024-01-04 15:52:05 +0100

    firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
+
 }
 
 android {
@@ -103,5 +109,23 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
     testImplementation("junit:junit:4.13.2")
+
+
+    dependencies {
+        // Import the Firebase BoM
+        implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+
+
+        // TODO: Add the dependencies for Firebase products you want to use
+        // When using the BoM, don't specify versions in Firebase dependencies
+        implementation("com.google.firebase:firebase-analytics")
+
+
+        // Add the dependencies for any other desired Firebase products
+        // https://firebase.google.com/docs/android/setup#available-libraries
+    }
+
+
+
 
 }
