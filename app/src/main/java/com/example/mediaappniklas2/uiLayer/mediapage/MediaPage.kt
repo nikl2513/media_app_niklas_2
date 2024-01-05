8b9df50a02578938bf40982fccf4d8f4776ea717
Nikolai Travis <s224567@student.dtu.dk>
2024-01-05 09:10:13 +0100

    rating scroll

--- app/src/main/java/com/example/mediaappniklas2/uiLayer/mediapage/MediaPage.kt
+++ app/src/main/java/com/example/mediaappniklas2/uiLayer/mediapage/MediaPage.kt
@@ -4,6 +4,7 @@ import android.widget.RatingBar
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -46,7 +47,9 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.mediaappniklas2.R
 import com.example.mediaappniklas2.navcontroller.Screen
 import kotlinx.coroutines.launch
-
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 
 @Composable
 fun MediaPageAPP(
@@ -95,13 +98,11 @@ fun TopmenuBar(
 @Composable
 fun MediaPage(modifier: Modifier = Modifier.background(Color.DarkGray), movieViewModel: MediaPageViewModel, viewModel: FilmRatingViewModel = FilmRatingViewModel()) {
 
-  //Mikkel Rating forsøg
+    //Mikkel Rating forsøg
     var rating by remember { mutableStateOf(0f) }
     var isRatingSubmitted by remember { mutableStateOf(false) }
     val scope = rememberCoroutineScope()
     var gennemsnitligRating by remember { mutableStateOf(0.0) }
-
-
 
 
     val currentMovie = movieViewModel.currentMovie.value
@@ -111,7 +112,6 @@ fun MediaPage(modifier: Modifier = Modifier.background(Color.DarkGray), movieVie
         }
     }
     val currentImdb = MediaPageViewModel.currentImdb
-
 
 
     // Use LaunchedEffect to observe changes in currentMovie
@@ -133,100 +133,105 @@ fun MediaPage(modifier: Modifier = Modifier.background(Color.DarkGray), movieVie
                 contentScale = ContentScale.Crop,
                 modifier = imageModifier
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Row(modifier = Modifier) {
-                Image(
-                    painter = painterResource(id = R.drawable.imdb),
-                    contentDescription = "logo",
-                    Modifier.width(35.dp)
-                )
-                Text(text = " ", color = Color.White)
-                Text(text = currentImdb.value?.averageRating.toString(), color = Color.White)
-                Text(text = "| ", color = Color.White)
-                Text(text = "4K ", color = Color.White)
-                Text(text = "| ", color = Color.White)
-                Text(text = "18 Years ", color = Color.White)
-                Text(text = "| ", color = Color.White)
-                Text(currentMovie.releasedate, color = Color.White)
-                Text(text = " | ", color = Color.White)
-                Text(text = "2 t. 32 m.", color = Color.White)
-                Text(text = " | ", color = Color.White)
-                Text(text = "avg. rating", color = Color.White)
-                Icon(
-                    imageVector = Icons.Default.Star, // This assumes you're using the default star icon
-                    contentDescription = null, // You can provide a description if needed
-                    tint = Color.White, // You can change the color of the star icon if required
-                    modifier = Modifier.padding(start = 4.dp))
-            }
-            Text(movie.title, color = Color.White)
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.TopCenter)
+                    .verticalScroll(rememberScrollState())
+                ,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
 
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row(modifier = Modifier) {
+                        Image(
+                            painter = painterResource(id = R.drawable.imdb),
+                            contentDescription = "logo",
+                            Modifier.width(35.dp)
+                        )
+                        Text(text = " ", color = Color.White)
+                        Text(
+                            text = currentImdb.value?.averageRating.toString(),
+                            color = Color.White
+                        )
+                        Text(text = "| ", color = Color.White)
+                        Text(text = "4K ", color = Color.White)
+                        Text(text = "| ", color = Color.White)
+                        Text(text = "18 Years ", color = Color.White)
+                        Text(text = "| ", color = Color.White)
+                        Text(currentMovie.releasedate, color = Color.White)
+                        Text(text = " | ", color = Color.White)
+                        Text(text = "2 t. 32 m.", color = Color.White)
 
-
-            LaunchedEffect(key1 = movie.title) {
-                // Hent gennemsnitlig rating ved opstart
-                gennemsnitligRating = viewModel.hentGennemsnitligFilmRating(movie.title)
-            }
-
-
-            Spacer(modifier = Modifier.height(20.dp))
-            Text("Gennemsnitlig Rating: $gennemsnitligRating")
-            if (!isRatingSubmitted) {
-                RatingBar(rating = rating, onRatingChanged = { newRating ->
-                    rating = newRating
-                })
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                // Gem rating knap
-                Button(onClick = {
-                    // Gem rating i Firebase
-
-                    scope.launch {
-                        // Gem rating i Firebase
-                        viewModel.gemFilmRating(movie.title, rating.toDouble())
-                        isRatingSubmitted = true
-                        gennemsnitligRating = viewModel.hentGennemsnitligFilmRating(movie.title)
-
-                    } }) {
-                    Text("Gem Rating")
-                }
-            } else {
-                // Vis besked, når rating er gemt
-                Text("Tak for din rating!")
-            }
-        }
-
-
-
-        }
-    }
-
-@Composable
-fun RatingBar(
-    rating: Float,
-    onRatingChanged: (Float) -> Unit
-) {
-    var isRatingBeingChanged by remember { mutableStateOf(false) }
-
-    Row(
-        modifier = Modifier
-            .wrapContentWidth()
-            .padding(8.dp)
-    ) {
-        for (i in 1..5) {
-            Icon(
-                imageVector = Icons.Default.Star,
-                contentDescription = null,
-                tint = if (isRatingBeingChanged || i <= rating) MaterialTheme.colorScheme.primary else Color.Gray,
-                modifier = Modifier
-                    .clickable {
-                        onRatingChanged(i.toFloat())
                     }
-                    .padding(4.dp)
-                    .size(40.dp)
-            )
+                    Text(movie.title, color = Color.White)
+
+                    LaunchedEffect(key1 = movie.title) {
+                        // Fetch average rating at startup
+                        gennemsnitligRating = viewModel.hentGennemsnitligFilmRating(movie.title)
+                    }
+
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Text("Gennemsnitlig Rating: $gennemsnitligRating")
+
+                    if (!isRatingSubmitted) {
+                        RatingBar(rating = rating, onRatingChanged = { newRating ->
+                            rating = newRating
+                        })
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Save rating button
+                        Button(onClick = {
+                            scope.launch {
+                                viewModel.gemFilmRating(movie.title, rating.toDouble())
+                                isRatingSubmitted = true
+                                gennemsnitligRating =
+                                    viewModel.hentGennemsnitligFilmRating(movie.title)
+                            }
+                        }) {
+                            Text("Save Rating")
+                        }
+                    } else {
+                        // Thank you message for rated movie
+                        Text("Thanks for your rating!")
+                    }
+                }
+            }
+
         }
     }
-}
+
+
+
+    @Composable
+    fun RatingBar(
+        rating: Float,
+        onRatingChanged: (Float) -> Unit
+    ) {
+        var isRatingBeingChanged by remember { mutableStateOf(false) }
+
+        Row(
+            modifier = Modifier
+                .wrapContentWidth()
+                .padding(8.dp)
+        ) {
+            for (i in 1..5) {
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = null,
+                    tint = if (isRatingBeingChanged || i <= rating) MaterialTheme.colorScheme.primary else Color.Gray,
+                    modifier = Modifier
+                        .clickable {
+                            onRatingChanged(i.toFloat())
+                        }
+                        .padding(4.dp)
+                        .size(40.dp)
+                )
+            }
+        }
+    }
+
 
 
