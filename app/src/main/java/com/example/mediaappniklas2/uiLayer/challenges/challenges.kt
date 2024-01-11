dd724a65cbaf9e0d3ddf0b27692ea6d7d2e04a3b
nikl2513 <nikl2513@outlook.dk>
2024-01-11 11:31:02 +0100

    adding challenges

---
+++ app/src/main/java/com/example/mediaappniklas2/uiLayer/challenges/challenges.kt
@@ -0,0 +1,182 @@
+package com.example.mediaappniklas2.uiLayer.challenges
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.mediaappniklas2.navcontroller.Screen
+import com.example.mediaappniklas2.uiLayer.startskærm.NavigationItem
+import com.example.mediaappniklas2.uiLayer.startskærm.Topapp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Challenges(
+    modifier: Modifier = Modifier
+        .background(Color.DarkGray)
+        .fillMaxSize()
+        .wrapContentSize(Alignment.TopCenter), navController: NavController,
+) {
+    val items = listOf(
+        NavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            route = Screen.Startskaerm.route
+            //tilføj route for navcontroller her
+        ),
+        NavigationItem(
+            title = "Search",
+            selectedIcon = Icons.Filled.Search,
+            unselectedIcon = Icons.Outlined.Search,
+            route = Screen.Search.route
+        ),
+        NavigationItem(
+            title = "List",
+            selectedIcon = Icons.Filled.List,
+            unselectedIcon = Icons.Outlined.List,
+            route = Screen.SavedMovieList.route
+        )/*,
+        NavigationItem(
+            title = "Your Streaming Services",
+            selectedIcon = Icons.Filled.PlayArrow,
+            unselectedIcon = Icons.Outlined.PlayArrow,
+            route = Screen.GradientButton.route
+        )*/
+
+    )
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.DarkGray
+    ) {
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        var selectedItemIndex by rememberSaveable {
+            mutableIntStateOf(0)
+        }
+        ModalNavigationDrawer(
+            drawerContent = {
+                ModalDrawerSheet {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    items.forEachIndexed { index, item ->
+                        NavigationDrawerItem(
+                            label = { Text(text = item.title) },
+                            selected = index == selectedItemIndex,
+                            onClick = {
+                                navController.navigate(item.route)
+                                //her kan man tilføje navcontroller.navigate(item.route)
+                                //hvor route er gemt i item
+                                selectedItemIndex = index
+                                scope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            icon = {
+                                Icon(
+                                    imageVector = if (index == selectedItemIndex) {
+                                        item.selectedIcon
+                                    } else item.unselectedIcon,
+                                    contentDescription = item.title
+                                )
+                            },
+                            badge = {
+                                item.badgeCount?.let {
+                                    Text(text = item.badgeCount.toString())
+                                }
+
+                            },
+                            modifier = Modifier
+                                .padding(NavigationDrawerItemDefaults.ItemPadding)
+                        )
+                    }
+
+                }
+            },
+            drawerState = drawerState
+        ) {
+            Row {
+                LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+                    item {
+                        Topapp(drawerState, navController)
+                        Text(text = "YOUR CHALLENGE", fontSize = 20.sp, color = Color.White)
+                        Text(text = "WATCH 10 MOVIES", fontSize = 20.sp, color = Color.White)
+                        LinearDeterminateIndicator(howLong = 50)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LinearDeterminateIndicator(howLong: Int) {
+    var currentProgress by remember { mutableStateOf(0f) }
+    val scope = rememberCoroutineScope() // Create a coroutine scope
+
+    // Start loading progress when composable enters the composition
+    LaunchedEffect(key1 = true) {
+        loadProgress({ progress ->
+            currentProgress = progress
+        }, howLong)
+    }
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        LinearProgressIndicator(
+            progress = currentProgress,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+/** Iterate the progress value */
+suspend fun loadProgress(updateProgress: (Float) -> Unit, howLong: Int) {
+    for (i in 1..howLong) {
+        updateProgress(i.toFloat() / 100)
+        delay(100)
+    }
+}
