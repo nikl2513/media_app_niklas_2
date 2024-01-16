0f97f93f13daa524d1110033d1efd4eaf9788bc8
MathiasLinde <113913029+MathiasLinde@users.noreply.github.com>
2024-01-16 16:46:56 +0100

    First Commit mockdataModule

---
+++ app/src/main/java/com/example/mediaappniklas2/datalayer/MockDataModule.kt
@@ -0,0 +1,20 @@
+package com.example.mediaappniklas2.datalayer
+
+
+
+fun createMockDataList() : List<MovieData>{
+
+
+   val list =  listOf<MovieData>(
+        MovieData("001","001","The Wolf of Wall Street","2014","https://www.movieposters.com/cdn/shop/products/wolfofwallstreet_480x.progressive.jpg?v=1620229322"),
+
+    )
+
+
+
+
+
+
+
+    return list
+}
