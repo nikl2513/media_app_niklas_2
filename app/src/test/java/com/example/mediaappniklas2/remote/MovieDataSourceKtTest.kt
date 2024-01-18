d28920bbb3a473dafaded2af9be331b832131024
nikl2513 <nikl2513@outlook.dk>
2024-01-18 13:43:17 +0100

    make the code prettier

--- app/src/test/java/com/example/mediaappniklas2/remote/MovieDataSourceKtTest.kt
+++ app/src/test/java/com/example/mediaappniklas2/remote/MovieDataSourceKtTest.kt
@@ -20,94 +20,64 @@ import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class MovieApiServiceTest {
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var movieApiService: MovieApiService
 
-        private lateinit var mockWebServer: MockWebServer
-        private lateinit var movieApiService: MovieApiService
+    @Before
+    fun setup() {
+        mockWebServer = MockWebServer()
+        movieApiService = RetrofitClient.movieApiService
+    }
 
-        @Before
-        fun setup() {
-            mockWebServer = MockWebServer()
-            movieApiService = RetrofitClient.movieApiService
-        }
-
-        @After
-        fun teardown() {
-            mockWebServer.shutdown()
-        }
-
-        @Test
-        fun `fetchMovies success response`() = runBlocking {
-
-                // Set up a mock response with a successful JSON body
-                val mockResponse = MockResponse()
-                    .setResponseCode(200)
-                    .setBody("{ \"results\": [{ \"title\": \"The Flash\", \"releasedate\": \"2022-01-01\", \"imageRef\": \"path/to/image.jpg\" }] }")
-
-                // Enqueue the mock response
-                mockWebServer.enqueue(mockResponse)
-
-                val movieApiResponse: MovieApiResponse = RetrofitClient.movieApiService.fetchMovies(1990, 2023)
-
-                // Process the response as before
-                val resultsList: List<MovieDTO> = movieApiResponse.results
-
-                // Map MovieDTO to MovieData
-                val movieDataList: List<MovieData> = resultsList.map { convertToMovieData(it) }
-
-                // Verify specific details about the movies
-                assertEquals(50, movieDataList.size)
-            assertEquals("61e58509d8f3c0931e423367",movieDataList.first().movieID)
-                assertEquals("The Flash", movieDataList.first().title)
-                assertEquals("2023", movieDataList.first().releasedate)
-                assertEquals("https://m.media-amazon.com/images/M/MV5BZWE2ZWE5MDQtMTJlZi00MTVjLTkxOTgtNmNiYjg2NDIxN2NhXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
-                    movieDataList.first().imageRef)
-
-                // Additional assertions or verifications if needed
-            }
-
-
-
-
-
-
+    @After
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
 
     @Test
-        fun searchMovie() = runBlocking{
-            val searchWord = "The Wolf of Wall Street" // Use the unencoded search word
-            val call: MovieApiResponse =
-                RetrofitClient.movieApiService.searchmovies(searchWord)
+    fun `fetchMovies success response`() = runBlocking {
+        val mockResponse = MockResponse()
+            .setResponseCode(200)
+            .setBody("{ \"results\": [{ \"title\": \"The Flash\", \"releasedate\": \"2022-01-01\", \"imageRef\": \"path/to/image.jpg\" }] }")
+        mockWebServer.enqueue(mockResponse)
+        val movieApiResponse: MovieApiResponse =
+            RetrofitClient.movieApiService.fetchMovies(1990, 2023)
+        val resultsList: List<MovieDTO> = movieApiResponse.results
+        val movieDataList: List<MovieData> = resultsList.map { convertToMovieData(it) }
+        assertEquals(50, movieDataList.size)
+        assertEquals("61e58509d8f3c0931e423367", movieDataList.first().movieID)
+        assertEquals("The Flash", movieDataList.first().title)
+        assertEquals("2023", movieDataList.first().releasedate)
+        assertEquals(
+            "https://m.media-amazon.com/images/M/MV5BZWE2ZWE5MDQtMTJlZi00MTVjLTkxOTgtNmNiYjg2NDIxN2NhXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+            movieDataList.first().imageRef
+        )
+    }
 
-            val searchApiResponse = call.results
-
-            if (searchApiResponse != null) {
-
-
-                if (searchApiResponse.isNotEmpty()) {
-                    val movieData: List<MovieData> = searchApiResponse.map { convertToMovieData(it) }
-                    val movieTitle: String = movieData.first().title
-                   println(movieTitle)
-                    println(searchWord)
-
-
-
-                    assertTrue(movieTitle == searchWord)
-                }
-
+    @Test
+    fun searchMovie() = runBlocking {
+        val searchWord = "The Wolf of Wall Street"
+        val call: MovieApiResponse =
+            RetrofitClient.movieApiService.searchmovies(searchWord)
+        val searchApiResponse = call.results
+        if (searchApiResponse != null) {
+            if (searchApiResponse.isNotEmpty()) {
+                val movieData: List<MovieData> = searchApiResponse.map { convertToMovieData(it) }
+                val movieTitle: String = movieData.first().title
+                println(movieTitle)
+                println(searchWord)
+                assertTrue(movieTitle == searchWord)
             }
         }
+    }
 
     @Test
     fun mediaRating_IsCorrect() = runBlocking {
         val movieID = ("tt0439572")
         val TheFlashRating = 6.7F
-
         MediaPageViewModel.searchRating(movieID)
         val MediaPageRating = MediaPageViewModel.currentImdb.value?.averageRating
-
         assertEquals(TheFlashRating, MediaPageRating)
     }
-
-
-
 }
 
